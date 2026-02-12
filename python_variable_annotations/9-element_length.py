88f9e9ede98491c10f36d09bd5e15f4367ce9d11
Your Name <you@example.com>
2026-02-13 01:37:37 +0400

    gitpusher

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"sjnfdsd"
+from typing import Sequence, Tuple
+
+
+def element_length(lst: Sequence) -> Tuple[float, int]:
+    "dfgsdfg"
+    return [(i, len(i)) for i in lst]
