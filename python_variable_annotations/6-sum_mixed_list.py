b7bc0bf3bf0f559f44e920f6d3377f38403da869
Your Name <you@example.com>
2026-02-13 01:24:12 +0400

    gitpusher

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"sjnfdsd"
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    "fsdfs"
+    res = 0
+    for i in mxd_lst:
+        res += i
+    return res
