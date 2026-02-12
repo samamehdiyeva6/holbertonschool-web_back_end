75959a1bf0eef9b989a90d003adca1edf3a5a8a7
Your Name <you@example.com>
2026-02-13 01:27:37 +0400

    gitpusher

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"sjnfdsd"
+import math
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Tuple[Union[int, float]]) -> tuple:
+    "dsdfds"
+    return (k, math.pow(v, 2))
