1a31d6f93bfedc610ef5e57d974f044208fa933a
Your Name <you@example.com>
2026-02-13 01:08:57 +0400

    gitpusher

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 "sjnfdsd"
 
 
-a:int = 1
-pi:float = 3.14
-i_understand_annotations:bool = True
-school:str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
