69045b84f15784c3c5850e63582fb5ae6c52b35b
Your Name <you@example.com>
2026-02-13 01:33:32 +0400

    gitpusher

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"sjnfdsd"
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    "sdfsfds"
+    def multiplier_function(x: float) -> float:
+        "dsfsdfds"
+        return x * multiplier
+    return multiplier_function
