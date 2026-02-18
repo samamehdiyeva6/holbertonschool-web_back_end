cbcffd18276e4360f598771a8f9596afdf71c66f
Your Name <you@example.com>
2026-02-18 18:10:56 +0400

    gitpusher

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@
 import asyncio
 import random
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10) -> int:
     "sdfsdfds"
     rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
