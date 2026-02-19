c3a87a4d4c09c9388069453e03585fe99ad06a91
Your Name <you@example.com>
2026-02-19 16:38:33 +0400

    gitpusher

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """sdfds"""
-    tasks = [task_wait_random(wait_random(max_delay)) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
 
     delays = []
     for completed in asyncio.as_completed(tasks):
