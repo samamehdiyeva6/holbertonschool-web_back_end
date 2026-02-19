fa1422f168ffc67e7eca6ce619dfd6aab27e27de
Your Name <you@example.com>
2026-02-19 15:55:45 +0400

    gitpusher

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """sdfds"""
     tasks = [wait_random(max_delay) for _ in range(n)]
