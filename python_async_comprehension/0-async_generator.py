23962aca07887c0106dbbd495d94d1aaaedaf6b4
Your Name <you@example.com>
2026-02-19 16:56:27 +0400

    gitpusher

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """"sdfsdfsd"""
     for i in range(10):
         await asyncio.sleep(1)
