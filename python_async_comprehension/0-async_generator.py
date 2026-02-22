0f15d7a03e43fbbb267904c7d2bd399ec980ec45
Your Name <you@example.com>
2026-02-22 22:13:23 +0400

    gitpusher

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""sdfsdfdsfsd"""
-
+"""Async generator that yields random numbers."""
 
 import asyncio
 import random
@@ -8,7 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """"sdfsdfsd"""
+    """Yield 10 random numbers between 0 and 10 asynchronously."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0,10)
+        yield random.uniform(0, 10)
