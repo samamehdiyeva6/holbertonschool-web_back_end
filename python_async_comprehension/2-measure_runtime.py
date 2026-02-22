eb8d2a190a23394bc6a03372e8705933c530cd4c
Your Name <you@example.com>
2026-02-22 23:11:10 +0400

    gitpusher

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measure the total runtime of async_comprehension."""
     start_time = time.time()
-    await asyncio.gather(async_comprehension() for _ in range(4))
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
     end_time = time.time()
     return end_time - start_time
