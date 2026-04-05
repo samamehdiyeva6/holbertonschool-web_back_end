5aa8401e89e62d3bbdb90d497869a2a5a2a01387
Your Name <you@example.com>
2026-04-05 15:01:34 +0400

    gitpusher

---
+++ i18n/2-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Flask-Babel is an extension to Flask that adds
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+def get_locale():
+    """Determines the best match with our supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(locale_selector=get_locale)
+babel.init_app(app)
