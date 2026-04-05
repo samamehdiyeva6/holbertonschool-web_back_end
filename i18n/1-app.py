5f22f5e03f6c0da1b78c3f6ebb97bc053da804ef
Your Name <you@example.com>
2026-04-05 14:34:13 +0400

    gitpusher

---
+++ i18n/1-app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from babel import Babel
+"""Flask-Babel is an extension to Flask that adds support for internationalization (i18n)"""
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config(object):
+    """Configuration class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+
+app.config.from_object(Config)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
