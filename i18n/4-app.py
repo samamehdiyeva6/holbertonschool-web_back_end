9c659dd52b300f92fb7aed3ecf6c246dc7417534
Your Name <you@example.com>
2026-04-05 16:50:19 +0400

    gitpusher

---
+++ i18n/4-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+This module is for Babel object instantiation
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    This class is for configuring the languages
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+def get_locale():
+    """
+    Determines the best match for supported languages
+    """
+    if request.args.get('locale'):
+        return request.args.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def home():
+    """
+    Renders the template
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
