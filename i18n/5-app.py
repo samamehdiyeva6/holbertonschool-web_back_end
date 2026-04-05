e65294619093103061697d8916aba02db17f259b
Your Name <you@example.com>
2026-04-05 17:34:27 +0400

    gitpusher

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -23,12 +23,14 @@ def get_locale():
         return request.args.get('locale')
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
 
 def get_user(login_as):
     """
