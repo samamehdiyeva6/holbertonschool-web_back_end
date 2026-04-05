ce942372c6e8d4af43bbbf1b044798390a389963
Your Name <you@example.com>
2026-04-05 16:37:33 +0400

    gitpusher

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -10,14 +10,21 @@ class Config:
     """
     This class is for configuring the languages
     """
-    LANGUAGES = ["fr", "en"]
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+def get_locale():
+    """
+    Determines the best match for supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
