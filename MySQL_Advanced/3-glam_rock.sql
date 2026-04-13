7cb11d4524cce9e03cd125dabbb3af95facef98c
Your Name <you@example.com>
2026-04-13 15:11:38 +0400

    done

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 --dsvdfgsdf
-SELECT band_name, IFNULL(split, 2024) - formed AS lifespan FROM metal_bands
-WHERE style = 'Glam rock'
+SELECT band_name, (IFNULL(split, 2024) - formed) AS lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
