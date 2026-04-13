a0d0c637b255693851a7036ecdc5821c4b2db43a
Your Name <you@example.com>
2026-04-13 15:08:24 +0400

    done

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 --dsvdfgsdf
-SELECT band_name, IFNULL(split, CURDATE()) - formed AS lifespan FROM metal_bands
+SELECT band_name, IFNULL(split, 2024) - formed AS lifespan FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
