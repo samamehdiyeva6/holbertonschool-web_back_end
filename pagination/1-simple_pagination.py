5cf86ed938a45e670fa8d46473d0989d26ed8816
Your Name <you@example.com>
2026-04-19 23:32:29 +0400

    gitpusher

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+from typing import Tuple
+import csv
+import math
+from typing import List
+
+
+def indef_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns page numbers to display in pagination."""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns the appropriate page of the dataset (i.e. the correct
+        list of rows).
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+
+        start, end = indef_range(page, page_size)
+        dataset = self.dataset()
+
+        if start >= len(dataset):
+            return []
+
+        return dataset[start:end]
