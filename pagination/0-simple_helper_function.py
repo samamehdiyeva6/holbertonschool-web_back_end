4896708e53028ecdfadb6682a9e5afb8b2b673b4
Your Name <you@example.com>
2026-04-22 16:24:39 +0400

    gitpusher

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,5 +8,5 @@ from typing import Tuple
 def indef_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns page numbers to display in pagination."""
     start = (page - 1) * page_size
-    end = start + page_size
+    end = page * page_size
     return (start, end)
